38e4aa1d4687ad314e268470add4dc54232d413c
Allysa5 <allysa_adams@yahoo.com>
2017-06-19 11:54:29 -0400

    MathTwo and ChangeProg first commit

---
+++ src/MathTwo.java
@@ -0,0 +1,25 @@
+
+public class MathTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int mynumber = 2;
+		double myothernumber = 1.7938;
+		
+	System.out.println("mynumber = " + mynumber + " myothernumber = " + myothernumber);
+	
+	
+	
+	// Assignment 2 Change program
+	
+	int x = 5;
+	double y= 9,z;
+	 z = x * y;
+	
+	System.out.println("the product is " + z);
+
+	}
+
+}
+
